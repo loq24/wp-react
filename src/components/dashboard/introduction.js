a3822e8cdcfc9e6d4a39ea267d2caebc25710789
LougieQ <lougie.quisel@gmail.com>
2019-03-05 15:17:55 +0800

    Update dashboard text

--- src/components/dashboard/introduction.js
+++ src/components/dashboard/introduction.js
@@ -4,8 +4,8 @@ class Introduction extends Component{
 	render(){
 		return(
 			<div>
-    			<p>This project is to serve as a guide for users that is looking for an idea on how to use Wordpress Rest API with React as a frontend. Improvements are welcome. You may create a pull request or just contact me at my <a target="_blank" href="https://lougiequisel.com/">personal website</a>.</p>
-                <p>I also created an OAuth 2.0 demo as well found here - <a href="https://github.com/loq24/oauth-demo" target="_blank">https://github.com/loq24/oauth-demo</a>.</p>
+    			<p>This project is to serve as a guide for users that is looking for an idea on how to use Wordpress Rest API with React as a frontend. Improvements are welcome. You may create a pull request or just contact me at my <a rel="noopener noreferrer" target="_blank" href="https://lougiequisel.com/">personal website</a>.</p>
+                <p>I also created an OAuth 2.0 demo as well found here - <a rel="noopener noreferrer" href="https://github.com/loq24/oauth-demo" target="_blank">https://github.com/loq24/oauth-demo</a>.</p>
     		</div>
 		);
 	}
